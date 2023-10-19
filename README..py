3b23ebc5d60af42b209d62fdb66ef76da9ba40f7
Rohan Chikorde <rohan.chikorde@gmail.com>
2023-10-20 00:30:24 +0530

    Update README..py

--- README..py
+++ README..py
@@ -1,6 +1,6 @@
 import xml.etree.ElementTree as ET
 
-xml_string = '''
+xml_string2 = '''
 <catalog xmlns:dc="http://example.com/dc" xmlns:ex="http://example.com/ex">
    <ex:book id="bk101">
       <author>
@@ -37,9 +37,46 @@ xml_string = '''
 </catalog>
 '''
 
+xml_string = '''
+<catalog xmlns:abc="http://example.com/abc" xmlns:xyz="http://example.com/xyz">
+   <xyz:book id="bk101">
+      <author>
+         <a>
+            <b>
+            <c>qef</c></b>
+         </a> 
+      </author>
+
+      <abc:title>XML Developer's Guide</abc:title>
+      <genre>Computer</genre>
+      <price>44.95</price>
+      <publish_date>2000-10-01</publish_date>
+      <description>An in-depth look at creating applications
+      with XML.</description>
+   </xyz:book>   
+   <xyz:apple id="bk102">
+      <abc:author>Ralls, Kim</abc:author>
+      <title>Midnight Rain</title>
+      <genre>Fantasy</genre>
+      <price>5.95</price>
+      <publish_date>2000-12-16</publish_date>
+      <description>A former architect battles corporate zombies,
+      an evil sorceress, and her own childhood to become queen
+      of the world.</description>
+      <xyz:longParent>
+         <xyz:longChild1>
+            <xyz:longChild2>
+               <abc:targetElement>Original Value</abc:targetElement>
+            </xyz:longChild2>
+         </xyz:longChild1>
+      </xyz:longParent>
+   </xyz:apple>
+</catalog>
+'''
+
 namespaces = {
-    'dc': 'http://example.com/dc',
-    'ex': 'http://example.com/ex'
+    'abc': 'http://example.com/abc',
+    'xyz': 'http://example.com/xyz'
 }
 
 def update_or_add_xml(xml_string, parent_path, tag, new_value, namespaces):
@@ -65,6 +102,6 @@ def update_or_add_xml(xml_string, parent_path, tag, new_value, namespaces):
     return ET.tostring(root, encoding='utf-8', method='xml').decode('utf-8')
 
 
-long_parent_path = './ex:apple/ex:longParent/ex:longChild1/ex:longChild2'
-updated_xml = update_or_add_xml(xml_string, long_parent_path, 'dc:targetElement2', 'New Value1', namespaces)
+long_parent_path = './xyz:apple/xyz:longParent/xyz:longChild1/xyz:longChild2'
+updated_xml = update_or_add_xml(xml_string, long_parent_path, 'abc:targetElement2', 'New Value1', namespaces)
 print(updated_xml)
